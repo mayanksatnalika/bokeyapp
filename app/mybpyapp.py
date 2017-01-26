9e3c6b133ec180c83c2232148caa62c12458aa84
mayanksatnalika <satnalikamayank12@gmail.com>
2017-01-26 12:24:46 +0530

    de

--- app/mybpyapp.py
+++ app/mybpyapp.py
@@ -1,121 +1,42 @@
-from os.path import dirname, join
+from random import random
 
-import numpy as np
-import pandas.io.sql as psql
-import sqlite3 as sql
+from bokeh.layouts import column
+from bokeh.models import Button
+from bokeh.palettes import RdYlBu3
+from bokeh.plotting import figure, curdoc
 
-from bokeh.plotting import figure
-from bokeh.layouts import layout, widgetbox
-from bokeh.models import ColumnDataSource, HoverTool, Div
-from bokeh.models.widgets import Slider, Select, TextInput
-from bokeh.io import curdoc
-from bokeh.sampledata.movies_data import movie_path
+# create a plot and style its properties
+p = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
+p.border_fill_color = 'black'
+p.background_fill_color = 'black'
+p.outline_line_color = None
+p.grid.grid_line_color = None
 
-conn = sql.connect(movie_path)
-query = open(join(dirname(__file__), 'query.sql')).read()
-movies = psql.read_sql(query, conn)
+# add a text renderer to out plot (no data yet)
+r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="20pt",
+           text_baseline="middle", text_align="center")
 
-movies["color"] = np.where(movies["Oscars"] > 0, "orange", "grey")
-movies["alpha"] = np.where(movies["Oscars"] > 0, 0.9, 0.25)
-movies.fillna(0, inplace=True)  # just replace missing values with zero
-movies["revenue"] = movies.BoxOffice.apply(lambda x: '{:,d}'.format(int(x)))
+i = 0
 
-with open(join(dirname(__file__), "razzies-clean.csv")) as f:
-    razzies = f.read().splitlines()
-movies.loc[movies.imdbID.isin(razzies), "color"] = "purple"
-movies.loc[movies.imdbID.isin(razzies), "alpha"] = 0.9
+ds = r.data_source
 
-axis_map = {
-    "Tomato Meter": "Meter",
-    "Numeric Rating": "numericRating",
-    "Number of Reviews": "Reviews",
-    "Box Office (dollars)": "BoxOffice",
-    "Length (minutes)": "Runtime",
-    "Year": "Year",
-}
+# create a callback that will add a number in a random location
+def callback():
+    global i
 
-desc = Div(text=open(join(dirname(__file__), "description.html")).read(), width=800)
+    # BEST PRACTICE --- update .data in one step with a new dict
+    new_data = dict()
+    new_data['x'] = ds.data['x'] + [random()*70 + 15]
+    new_data['y'] = ds.data['y'] + [random()*70 + 15]
+    new_data['text_color'] = ds.data['text_color'] + [RdYlBu3[i%3]]
+    new_data['text'] = ds.data['text'] + [str(i)]
+    ds.data = new_data
 
-# Create Input controls
-reviews = Slider(title="Minimum number of reviews", value=80, start=10, end=300, step=10)
-min_year = Slider(title="Year released", start=1940, end=2014, value=1970, step=1)
-max_year = Slider(title="End Year released", start=1940, end=2014, value=2014, step=1)
-oscars = Slider(title="Minimum number of Oscar wins", start=0, end=4, value=0, step=1)
-boxoffice = Slider(title="Dollars at Box Office (millions)", start=0, end=800, value=0, step=1)
-genre = Select(title="Genre", value="All",
-               options=open(join(dirname(__file__), 'genres.txt')).read().split())
-director = TextInput(title="Director name contains")
-cast = TextInput(title="Cast names contains")
-x_axis = Select(title="X Axis", options=sorted(axis_map.keys()), value="Tomato Meter")
-y_axis = Select(title="Y Axis", options=sorted(axis_map.keys()), value="Number of Reviews")
+    i = i + 1
 
-# Create Column Data Source that will be used by the plot
-source = ColumnDataSource(data=dict(x=[], y=[], color=[], title=[], year=[], revenue=[], alpha=[]))
+# add a button widget and configure with the call back
+button = Button(label="Press Me")
+button.on_click(callback)
 
-hover = HoverTool(tooltips=[
-    ("Title", "@title"),
-    ("Year", "@year"),
-    ("$", "@revenue")
-])
-
-p = figure(plot_height=600, plot_width=700, title="", toolbar_location=None, tools=[hover])
-p.circle(x="x", y="y", source=source, size=7, color="color", line_color=None, fill_alpha="alpha")
-
-
-def select_movies():
-    genre_val = genre.value
-    director_val = director.value.strip()
-    cast_val = cast.value.strip()
-    selected = movies[
-        (movies.Reviews >= reviews.value) &
-        (movies.BoxOffice >= (boxoffice.value * 1e6)) &
-        (movies.Year >= min_year.value) &
-        (movies.Year <= max_year.value) &
-        (movies.Oscars >= oscars.value)
-    ]
-    if (genre_val != "All"):
-        selected = selected[selected.Genre.str.contains(genre_val)==True]
-    if (director_val != ""):
-        selected = selected[selected.Director.str.contains(director_val)==True]
-    if (cast_val != ""):
-        selected = selected[selected.Cast.str.contains(cast_val)==True]
-    return selected
-
-
-def update():
-    df = select_movies()
-    x_name = axis_map[x_axis.value]
-    y_name = axis_map[y_axis.value]
-
-    p.xaxis.axis_label = x_axis.value
-    p.yaxis.axis_label = y_axis.value
-    p.title.text = "%d movies selected" % len(df)
-    source.data = dict(
-        x=df[x_name],
-        y=df[y_name],
-        color=df["color"],
-        title=df["Title"],
-        year=df["Year"],
-        revenue=df["revenue"],
-        alpha=df["alpha"],
-    )
-
-controls = [reviews, boxoffice, genre, min_year, max_year, oscars, director, cast, x_axis, y_axis]
-for control in controls:
-    control.on_change('value', lambda attr, old, new: update())
-
-sizing_mode = 'fixed'
-sizing_mode = 'scale_width'
-
-# 'scale_width' also looks nice with this example
-
-inputs = widgetbox(*controls, sizing_mode=sizing_mode)
-l = layout([
-    [desc],
-    [inputs, p],
-], sizing_mode=sizing_mode)
-
-update()  # initial load of the data
-
-curdoc().add_root(l)
-curdoc().title = "Movies"
+# put the button and plot in a layout and add to the document
+curdoc().add_root(column(button, p))
